ce2874ff58b09f67f49778fb7a127b0fb4f32d74
ph@MacPro <astroh.peng@gmail.com>
2019-04-08 10:11:30 -0400

    make it run under macOS

---
+++ config.py
@@ -0,0 +1,5 @@
+MATLAB_LOCATION = "/Applications/MATLAB_R2017b.app/bin/matlab"
+GPML_PATH = "gpml/"
+COLOR_SCHEME = "dark"
+LOCATION = "local"
+
--- examples/fast_example.py
+++ examples/fast_example.py
@@ -7,7 +7,7 @@ Experiment(description='An example experiment',
            k=1,                        # Keep the k best kernels at every iteration.  1 => greedy search.
            n_rand=2,                   # Number of random restarts.
            iters=10,                   # How long to optimize hyperparameters for.
-           base_kernels='SE,Per,Lin',
+           base_kernels='SE',
            verbose=True,
            make_predictions=False,
            skip_complete=False)
